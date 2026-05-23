ead506f2efcf4d96d29bbdc37ac255889af18daa
Pedro Neto <pednetocn@gmail.com>
2026-05-22 23:06:04 -0300

    fix: setup graphql module and prisma v7 client configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,20 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
+import { AppResolver } from './app.resolver';
 import { PrismaModule } from './shared/database/prisma.module';
 
 @Module({
   imports: [
     PrismaModule,
-    PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: join(process.cwd(), 'docs/graphql/schema.gql'),
       sortSchema: true,
+      playground: Boolean(process.env.GRAPHQL_PLAYGROUND),
+      introspection: Boolean(process.env.GRAPHQL_PLAYGROUND),
     }),
   ],
+  providers: [AppResolver],
 })
 export class AppModule {}
